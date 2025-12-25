e714a5927d643cf87c3e4e1acac50005945028a0
Yuan Chiu <chyuaner@gmail.com>
2025-12-25 16:49:49 +0800

    微調預設值與陰影調淡

--- src/core/components/BgElement.tsx
+++ src/core/components/BgElement.tsx
@@ -73,8 +73,8 @@ export const BgElement: React.FC<BgElementProps> = ({
       position: "absolute",
       filter:
         typeof shadow === "number"
-          ? `drop-shadow(0 0 ${shadow}px #00000090)`
-          : `drop-shadow(0 0 ${shadow} #00000090)`,
+          ? `drop-shadow(0 0 ${shadow}px #00000050)`
+          : `drop-shadow(0 0 ${shadow} #00000050)`,
       pointerEvents: "none", // 防止遮住滑鼠事件
       zIndex: 0,
       ...(radius !== undefined
